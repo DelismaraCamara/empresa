a10b6528bdf083e3b03af9c3730687381af4fcdf
DelismaraCamara <delismaracamara@gmail.com>
2020-09-17 02:58:11 +0200

    first commit

---
+++ src/app/footer/footer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CopyrightComponent } from './copyright/copyright.component';
+
+
+
+@NgModule({
+  declarations: [CopyrightComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FooterModule { }
